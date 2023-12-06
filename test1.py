64543e77033cd78212d0158270d363b86ff45499
Prince Francis <86868262+princefrancis64@users.noreply.github.com>
2023-12-06 23:43:50 +0530

    6/12/2023

--- test1.py
+++ test1.py
@@ -1,28 +1,90 @@
-import pymongo
+# from utls.utill import person2
 
-from pymongo.mongo_client import MongoClient
+# obj = person2("Priya","Annies",1999)
+# print(obj._name1)
+# print(obj._person2__surname1)
+# print(obj.yob1)
 
-uri = "mongodb+srv://princefrancis64:Oejb2e2l74Gz4NAK@cluster0.o5qm0dq.mongodb.net/?retryWrites=true&w=majority"
+# class Person:
+#     def __init__(self,name,surname,yob):
+#         self._name1 = name
+#         self.__surname1 = surname
+#         self.yob1 = yob
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
+# Prince = Person("Prince","Francis",1996)
+# print(Prince._name1)
+# print(Prince._Person__surname1)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
-db =client.test
-print(db)
+# def scope_test():
+#     def do_local():
+#         spam = "local spam"
 
-d = {
-    "name":"prince",
-    "email":"prince.francis64@gmail.com",
-    "surname":"francis"
-}
+#     def do_nonlocal():
+#         nonlocal spam
+#         spam = "nonlocal spam"
 
-db1 = client['mongotest']
-coll = db1['test']
-coll.insert_one(d)
+#     def do_global():
+#         global spam
+#         spam = "global spam"
+
+#     spam = "test spam"
+#     do_local()
+#     print("After local assignment:",spam)
+#     do_nonlocal()
+#     print("After nonlocal assignment:",spam)
+#     do_global()
+#     print("After gloabl assignment:",spam)
+
+# scope_test()
+# print("In global scope:",spam)
+
+# class Dog:
+#     def __init__(self,name):
+#         self.name = name
+#         self.tricks = [] 
+
+#     def add_trick(self,trick):
+#         return self.tricks.append(trick)
+    
+# d = Dog("Fido")
+# e = Dog("Buddy")
+# d.add_trick("roll over")
+# e.add_trick("Play dead")
+# print(d.tricks)
+# print(e.tricks)
+
+# from dataclasses import dataclass
+
+# @dataclass
+# class Employee:
+#     name:str
+#     dept:str
+#     salary:int
+
+# john = Employee("John","Mechanical",40000)
+# print(john.dept)
+# print(john.salary)
+
+import pandas as pd
+import json
+
+# df = pd.read_csv("diabetes.csv")
+# df = df.loc[:11,:]
+# # print(df.T)
+# s = list(json.loads(df.T.to_json()).values())
+# print(s)
+
+
+# data = {'Name': ['Alice', 'Bob', 'Charlie'],
+#         'Age': [25, 30, 35],
+#         'City': ['New York', 'San Francisco', 'Los Angeles']}
+
+# df = pd.DataFrame(data)
+# transposed_dict = df.T.to_json()
+
+# python_obj = json.loads(transposed_dict)
+# print(python_obj)
+
+l = list({"name":"Prince","father":"Francis","dob":1996}.values())
+print(l)
--- test2.py
+++ test2.py
@@ -1,49 +1,37 @@
-import pymongo
+##variable declaration without using init method
+import test1
+print(test1)
+obj3 = test1.Person("Prince","Francis",1990)
+print(obj3.yob1)
 
-from pymongo.mongo_client import MongoClient
+class person:
+    _name = "Prince"
+    __surname = "Francis"
+    yob = 1996
 
-uri = "mongodb+srv://princefrancis64:Oejb2e2l74Gz4NAK@cluster0.o5qm0dq.mongodb.net/?retryWrites=true&w=majority"
+    def _age(self,current_year):
+        return current_year - self.yob
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
+    def __age1(self,current_year):
+        return current_year - self.yob
+    
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-db =client.test
-# print(db)
+obj = person()
+# print(obj)
+# print(obj._age(2023))
+# print(obj._person__age1(2023))
 
-data = {
-    "name ":"prince",
-    "email_id":"prince.francis64@gmail.com",
-    "subject":["data science","big data","data analytics"]
-}
+class employee(person):
+    _name ="Francis"
+    __surname = "AL"
+    yob = 1966
 
-list_of_records = [
-    {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'},
 
-    {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Deep Learning for NLP and Computer vision'},
-
-    {'companyName': 'iNeuron',
-     'product': 'Master Program',
-     'courseOffered': 'Data Science Masters Program'}
-]
-
-database = client['myinfo5']
-collection = database["prince"]
-# collection.insert_one(data)
-collection.insert_many(list_of_records)
-
-collection1  = database['dpkt']
-data1 = {"packetType":"D","data":{"checkEngineLightFlag":"F","batteryVoltageStableTime":0,"batteryVoltageStable":"0","batteryVoltageOff":"12.42","batteryCrankParamTN":"-0.08","batteryCrankParamVN":"0.00","batteryCrankParamTP":"-0.08","batteryCrankParamVP":"0.00","batteryCrankParamTT":"-0.00008","batteryCrankParamV0":"0.00","batteryVoltageMaxOn":"13.05","batteryVoltageMinOn":"12.97","batteryVoltageMaxOff":"12.46","batteryVoltageMinOff":"12.36","batteryVoltageOnAverage":"13.02","engineLoadMax":"84","engineLoadAverage":"39.98","rpmMax":"3487","rpmAverage":"1431.29","gpsSpeedAverage":"21.99","vssMax":"53.44","vssAverage":"23.06","tcuTemperatureMin":"82.40","tcuTemperatureMax":"109.40","tcuTemperatureAverage":"104.87","coolantMin":"158.00","coolantMax":"188.60","coolantAverage":"180.20","packetStartLocal":1508143346000,"tripStartLocal":1508143346000,"milIndicator":"F","monitorsNotReady":0,"imei":"60DF5417","gatewayTs":1515613306592,"diagnosticTroubleCodeData":[345345],"diagnosticPidData":[[64768,47,100],[64768,1,517376],[64800,1,262144],[64768,5,125]]},"header":{"iwrapVer":"1.9.20","sourceSystem":"CDP","configVer":"1.1","oemName":"HUM","unitType":0,"cpVer":"7.50.1.9","igpsVer":"1.3.7","messageType":"Notification","pomVer":"1.0","headerVer":"V6","timestamp":0,"deviceType":"InDrive","visorVer":"1.4.35","transactionId":"53098471-7787-4160-94b3-cd69dcc70416","deviceSerialNo":"60DF5417","subOrganization":"HUM","organization":"HUM","imei":"60DF5417","operation":"Notification"}}
-collection1.insert_one(data1)
-record = collection.find()
-for i in record:
-    print(i)
+obj1 = employee()
+# print(obj1._age(2023))
+# print(obj1._person__age1(2023))
+# print(obj1)
+# print(obj1.yob)
+# print(obj1._person__surname)
+# print(obj1._name)
+# print(obj1._employee__surname)
