b2c46a8556e2daa0b26cb4f33fabfc147bd4f938
princefrancis64 <prince.francis64@gmail.com>
2023-12-20 23:01:06 +0530

    data transformation completed

--- Dec_19/sensor/components/data_transformation.py
+++ Dec_19/sensor/components/data_transformation.py
@@ -10,6 +10,7 @@ import numpy as np
 from sensor import utils
 from sensor.config import TARGET_COLUMN
 from sklearn.preprocessing import LabelEncoder
+from imblearn.combine import SMOTETomek
 
 
 class DataTransformation:
@@ -33,15 +34,18 @@ class DataTransformation:
                 ('Imputer',simple_imputer),
                 ('RobustScaler',robust_scaler)
             ])
+            return pipeline
         except Exception as e:
             raise SensorException(e,sys)
     
 
-    def initiate_data_transformation(self,)->artifact_entity.DataTransformationArtifact:
+    def initiate_data_transformation(self)->artifact_entity.DataTransformationArtifact:
         try:
             #reading training and testing file
+            logging.info("Reading train and test df")
             train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
             test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            logging.info("Successfully read the dataset")
 
             #selecting input features for train and test dataframe
             input_feature_train_df = train_df.drop(TARGET_COLUMN,axis=1)
@@ -56,15 +60,38 @@ class DataTransformation:
 
             #transformation on target columns
             target_feature_train_arr = label_encoder.transform(target_feature_train_df)
-            target_feature_test_arrr = label_encoder.transform(target_feature_test_df)
+            target_feature_test_arr = label_encoder.transform(target_feature_test_df)
 
             transformation_pipeline = DataTransformation.get_data_transformer_object()
             transformation_pipeline.fit(input_feature_train_df)
 
             #transforming input features
-            input_feature_train_arrr  =transformation_pipeline.transform(input_feature_train_df)
+            input_feature_train_arr  =transformation_pipeline.transform(input_feature_train_df)
             input_feature_test_arr = transformation_pipeline.transform(input_feature_test_df)
 
-            
+            smt = SMOTETomek(random_state = 42)
+            logging.info(f"Before resampling in training dataset: Input features{input_feature_train_arr.shape} Target:{target_feature_train_arr.shape} ")
+            input_feature_train_arr,target_feature_train_arr = smt.fit_resample(input_feature_train_arr,target_feature_train_arr)
+            logging.info(f"Before resampling in testing dataset: Input features{input_feature_test_arr.shape} Target:{target_feature_test_arr.shape} ")
+            input_feature_test_arr,target_feature_test_arr = smt.fit_resample(input_feature_test_arr,target_feature_test_arr)
+
+            #encoded test and train arrays to be stored in transformed as train.npz and test.npz
+            train_arr = np.c_[input_feature_train_arr,target_feature_train_arr]
+            test_arr = np.c_[input_feature_test_arr,target_feature_test_arr]
+
+            #saving both the files in the transformed folder
+            utils.save_numpy_array(file_path=self.data_transformation_config.transformed_train_path,array = train_arr)
+            utils.save_numpy_array(file_path=self.data_transformation_config.transformed_test_path,array = test_arr)
+
+            utils.save_object(file_path=self.data_transformation_config.transform_object_path,obj = transformation_pipeline)
+            utils.save_object(file_path=self.data_transformation_config.target_encoder_path,obj = label_encoder)
+
+            data_transformation_artifact = artifact_entity.DataTransformationArtifact(
+                transform_object_path=self.data_transformation_config.transform_object_path,
+                transformed_train_path=self.data_transformation_config.transformed_train_path,
+                transformed_test_path=self.data_transformation_config.transformed_test_path,
+                target_encoder_path=self.data_transformation_config.target_encoder_path
+            )
+            return data_transformation_artifact
         except Exception as e:
             raise SensorException(e,sys)
--- Dec_19/sensor/utils.py
+++ Dec_19/sensor/utils.py
@@ -4,6 +4,8 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 import pandas as pd
 import yaml
+import numpy as np
+import dill
 
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
@@ -45,3 +47,34 @@ def write_yaml_file(file_path:str,data:dict):
             yaml.dump(data,file_writer)
     except Exception as e:
         raise SensorException(e,sys)
+    
+
+def save_numpy_array(file_path:str,array:np.array):
+    """
+    Save numpy array data to file
+    file_path"str location of file to save
+    array:np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_writer:
+            np.save(file_writer,array)
+    except Exception as e:
+        raise SensorException(e,sys)
+    
+def save_object(file_path:str,obj:object):
+    try:
+        logging.info("Entered the save object method of utils")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+        logging.info(f"{obj} Object has been successfully saved and exiting now")        
+    except Exception as e:
+        raise SensorException(e,sys)
+    
+# def load_object(file_path:str,obj:object):
+#     try:    
+
+#     except Exception as e:
+#         raise SensorException(e,sys)
