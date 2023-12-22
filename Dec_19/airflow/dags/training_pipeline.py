b1953745a848fdc53abbdc9472ebac49a6677888
princefrancis64 <prince.francis64@gmail.com>
2023-12-22 18:35:11 +0530

    project updated

--- Dec_19/airflow/dags/training_pipeline.py
+++ Dec_19/airflow/dags/training_pipeline.py
@@ -13,7 +13,7 @@ with DAG(
     # [END default_args]
     description='Sensor Fault Detection',
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2023, 12, 17, tz="UTC"),
+    start_date=pendulum.datetime(2023, 12, 22, tz="UTC"),
     catchup=False,
     tags=['example'],
 ) as dag:
